caa2e223586874a57e659a64af61686a7b3dab7e
28niwa2108 <fle20-account@yahoo.co.jp>
2021-11-20 16:38:20 +0900

    nicknameカラムバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: {maximum:6}
 end
