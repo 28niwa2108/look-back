503d4934b0e141d2f614c7241843e4e3d144da42
28niwa2108 <fle20-account@yahoo.co.jp>
2021-11-20 19:22:01 +0900

    テストコードを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,25 +55,25 @@ RSpec.describe 'ユーザー新規登録', type: :model do
     it 'emailが空では登録できない' do
       @user.email = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
+      expect(@user.errors.full_messages).to include("Eメールを入力してください")
     end
 
     it 'passwordが空では登録できない' do
       @user.password = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
+      expect(@user.errors.full_messages).to include("パスワードを入力してください")
     end
 
     it 'password_confirmationが空では登録できない' do
       @user.password_confirmation = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      expect(@user.errors.full_messages).to include("パスワード(確認用)とパスワードの入力が一致しません")
     end
 
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      expect(@user.errors.full_messages).to include("ニックネームを入力してください")
     end
 
     it 'emailが重複してる場合は登録できない' do
@@ -81,61 +81,61 @@ RSpec.describe 'ユーザー新規登録', type: :model do
       user2 = FactoryBot.build(:user)
       user2.email = @user.email
       user2.valid?
-      expect(user2.errors.full_messages).to include('Email has already been taken')
+      expect(user2.errors.full_messages).to include('Eメールはすでに使用されています')
     end
 
     it 'emailに@が含まれない場合は登録できない' do
       @user.email = 'abcdefg.com'
       @user.valid?
-      expect(@user.errors.full_messages).to include('Email is invalid')
+      expect(@user.errors.full_messages).to include('Eメールは不正な値です')
     end
 
     it 'passwordが6文字未満では登録できない' do
       @user.password = 'a2345'
       @user.password_confirmation = @user.password
       @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
     end
 
     it 'passwordが128文字超過では登録できない' do
       @user.password = Faker::Lorem.characters(number: 129, min_alpha: 1, min_numeric: 1)
       @user.password_confirmation = @user.password
       @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
+      expect(@user.errors.full_messages).to include('パスワードは128文字以内で入力してください')
     end
 
     it 'passwordが全角文字を含む場合は登録できない' do
       @user.password = 'Ａ２3456'
       @user.password_confirmation = @user.password
       @user.valid?
-      expect(@user.errors.full_messages).to include('Password is invalid')
+      expect(@user.errors.full_messages).to include('パスワードは不正な値です')
     end
 
     it 'passwordが半角英字を含まないと登録できない' do
       @user.password = '123456'
       @user.password_confirmation = @user.password
       @user.valid?
-      expect(@user.errors.full_messages).to include('Password is invalid')
+      expect(@user.errors.full_messages).to include('パスワードは不正な値です')
     end
 
     it 'passwordが半角数字を含まないと登録できない' do
       @user.password = 'abcdef'
       @user.password_confirmation = @user.password
       @user.valid?
-      expect(@user.errors.full_messages).to include('Password is invalid')
+      expect(@user.errors.full_messages).to include('パスワードは不正な値です')
     end
 
     it 'passwordとpassword_confirmationが一致しないと登録できない' do
       @user.password = 'a23456'
       @user.password_confirmation = 'b23456'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      expect(@user.errors.full_messages).to include("パスワード(確認用)とパスワードの入力が一致しません")
     end
 
     it 'nicknameが6文字超過では登録できない' do
       @user.nickname = '1234567'
       @user.valid?
-      expect(@user.errors.full_messages).to include('Nickname is too long (maximum is 6 characters)')
+      expect(@user.errors.full_messages).to include('ニックネームは6文字以内で入力してください')
     end
   end
 end
