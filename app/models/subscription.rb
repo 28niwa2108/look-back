bf7f6a018bf1547b701f435721cd4a77567338fa
28niwa2108 <fle20-account@yahoo.co.jp>
2021-11-23 08:38:50 +0900

    バリデーション・アソシエーションの設定

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,10 @@
 class Subscription < ApplicationRecord
+  with_options presence: true do
+    validates :name, length: { maximum: 10 }
+    validates :price, numericality: { only_integer: true }
+    validates :contact_date
+    validates :update_type_id, numericality: {other_than: 0 }
+    validates :update_cycle, numericality: { only_integer: true }
+  end
+  belongs_to :user
 end
