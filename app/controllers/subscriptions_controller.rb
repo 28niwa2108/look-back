ad84c0bea695193f36eb506773a1e756e8319367
28niwa2108 <fle20-account@yahoo.co.jp>
2021-11-23 16:06:56 +0900

    new, createアクション実装

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,39 @@
 class SubscriptionsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :user_identification, except: [:index]
+
+  def new
+    set_user
+    set_subs
+  end
+
+  def create
+    @subs = Subscription.new(subs_params)
+    if @subs.valid?
+      @subs.save
+      redirect_to user_path(current_user)
+    else
+      set_user 
+      render 'subscriptions/new'
+    end
+  end
+
+  private
+  def user_identification
+    redirect_to root_path if current_user != User.find(params[:user_id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_subs
+    @subs = Subscription.new
+  end
+
+  def subs_params
+    params.require(:subscription).permit(
+      :name, :price, :contract_date, :update_type_id, :update_cycle
+    ).merge(user_id: current_user.id)
+  end
 end
