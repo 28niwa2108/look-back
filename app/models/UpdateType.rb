56b8a2080d99de98bf53c2e723e2f56238c92900
28niwa2108 <fle20-account@yahoo.co.jp>
2021-11-23 16:02:16 +0900

    モデル誤記修正

--- app/models/UpdateType.rb
+++ app/models/UpdateType.rb
@@ -1,9 +1,9 @@
 class UpdateType < ActiveHash::Base
   self.data = [
-    { id: 0, name: '--' },
     { id: 1, name: '日' },
     { id: 2, name: '月' },
     { id: 3, name: '年' }
   ]
-  has_many: subscriptions
+  include ActiveHash::Associations
+  has_many :subscriptions
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,11 +2,13 @@ class Subscription < ApplicationRecord
   with_options presence: true do
     validates :name, length: { maximum: 10 }
     validates :price, numericality: { only_integer: true }
-    validates :contact_date
+    validates :contract_date
     validates :update_cycle, numericality: { only_integer: true }
   end
-  validates :update_type_id, numericality: {other_than: 0 }
+  validates :update_type_id, numericality: { only_integer: true }
 
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :update_type
 end
