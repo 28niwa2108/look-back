4e0448687d1e5dec99866fcd8fd65684df658c21
28niwa2108 <fle20-account@yahoo.co.jp>
2021-11-26 22:52:44 +0900

    Subscriptionモデル：バリデーションの改善

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,9 +3,17 @@ class Subscription < ApplicationRecord
     validates :name, length: { maximum: 10 }
     validates :price, numericality: { only_integer: true }
     validates :contract_date
-    validates :update_cycle, numericality: { only_integer: true }
+    validates :update_cycle, numericality: { 
+      only_integer: true,
+      greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 31
+  }
   end
-  validates :update_type_id, numericality: { only_integer: true }
+  validates :update_type_id, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 3
+  }
 
   belongs_to :user
 
