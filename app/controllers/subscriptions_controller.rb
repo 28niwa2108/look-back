3e34707f40c580f5cb0a20278ae38afb62f274ff
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-02 23:05:08 +0900

    詳細・編集・削除機能追加

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,16 +1,20 @@
 class SubscriptionsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index]
   before_action :user_identification, except: [:index]
 
-  def new
+  def show
     set_user
     set_subs
   end
 
+  def new
+    set_user
+    @subs = Subscription.new
+  end
+
   def create
     @subs = Subscription.new(subs_params)
-    if @subs.valid?
-      @subs.save
+    if @subs.save
       redirect_to user_path(current_user)
     else
       set_user
@@ -18,10 +22,30 @@ class SubscriptionsController < ApplicationController
     end
   end
 
+  def edit
+    set_user
+    set_subs
+  end
+
+  def update
+    set_subs
+    if @subs.update(subs_params)
+      redirect_to user_path(current_user)
+    else
+      set_user
+      render :edit
+    end
+  end
+
+  def destroy
+    set_subs
+    redirect_to user_path(current_user) if @subs.destroy
+  end
+
   private
 
   def user_identification
-    redirect_to root_path if current_user != User.find(params[:user_id])
+    redirect_to user_path(current_user) if current_user != User.find_by(id: params[:user_id])
   end
 
   def set_user
@@ -29,7 +53,8 @@ class SubscriptionsController < ApplicationController
   end
 
   def set_subs
-    @subs = Subscription.new
+    @subs = Subscription.find_by(id: params[:id])
+    redirect_to user_path(current_user) if @subs.nil?
   end
 
   def subs_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'subscriptions#index'
   devise_for :users
   resources :users, only: [:show] do
-    resources :subscriptions, only: [:new, :create]    
+    resources :subscriptions, only: [:show, :new, :create, :edit, :update, :destroy]    
   end
 end
