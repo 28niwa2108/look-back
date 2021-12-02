525393e83040e272a8f506100ba51bbe8fa6fee1
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-02 23:07:43 +0900

    Rubocop指摘分修正

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,11 +3,11 @@ class Subscription < ApplicationRecord
     validates :name, length: { maximum: 10 }
     validates :price, numericality: { only_integer: true }
     validates :contract_date
-    validates :update_cycle, numericality: { 
+    validates :update_cycle, numericality: {
       only_integer: true,
       greater_than_or_equal_to: 1,
       less_than_or_equal_to: 31
-  }
+    }
   end
   validates :update_type_id, numericality: {
     only_integer: true,
