0f5521ff1ddd23063d6dd004f8eef0a768d183e3
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-11 18:35:19 +0900

    アソシエーション修正

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,5 @@
 class Subscription < ApplicationRecord
+  #バリデーション
   with_options presence: true do
     validates :name, length: { maximum: 10 }
     validates :price, numericality: { only_integer: true }
@@ -22,13 +23,15 @@ class Subscription < ApplicationRecord
     less_than_or_equal_to: 2
   }, unless: :type_is_day
 
+  #アソシエーション
   belongs_to :user
-  has_one :contract_renewal
+  has_one :contract_renewal, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :update_type
   belongs_to :update_day_type
 
+  #メソッド
   def type_is_day
     self.update_type_id == 1
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :nickname, presence: true, length: { maximum: 6 }
+  has_many :subscriptions, dependent: :destroy
 end
