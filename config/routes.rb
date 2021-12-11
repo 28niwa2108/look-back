4ce245e75f9f9209aacbc6916e45ba7d57e383d5
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-11 18:37:11 +0900

    軽微な修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'subscriptions#index'
   devise_for :users
   resources :users, only: [:show] do
-    resources :subscriptions, only: [:show, :new, :create, :edit, :update, :destroy]    
+    resources :subscriptions, only: [:show, :new, :create, :edit, :update, :destroy]
   end
 end
