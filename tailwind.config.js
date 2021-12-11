9caffe0f999e5e98cec70b56b14bef7632190d2b
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-11 21:33:22 +0900

    tailwind設定拡張

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["app/views/*/*.erb", "app/views/*/*/*.erb","app/javascript/delete_alert.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '400px': '400px'
+      }
+    },
   },
   variants: {
     extend: {},
