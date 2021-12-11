e305e79c71a36db33f7b29b4f624d41a6256d805
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-11 18:37:44 +0900

    active_supportライブラリの追加

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'date'
+require 'active_support'
+require 'active_support/core_ext'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
