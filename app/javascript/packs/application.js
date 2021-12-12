e8a4960b90c49bf3dc112a96d7896f425098bd03
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-12 22:23:48 +0900

    更新機能を追加(フロント)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,11 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../sub_update.js")
 require("../delete_alert.js")
 require("../sub_form.js")
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
