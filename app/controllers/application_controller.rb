7ce9eed8fc9ea826897ac8feb55b90c7ccabced2
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-12 22:18:24 +0900

    tailwind参照ファイルの記述変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,21 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
+  #Basic認証
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
+  #devise 登録時の許可パラメーターにnicknameカラム追加
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
+  #ログイン後はマイページへ遷移
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
+
 end
