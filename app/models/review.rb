8adc0c90993fcd987252f1c5bd56616eb0972f13
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-17 22:10:08 +0900

    バリデーション・アソシエーション定義

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ApplicationRecord
+  validates :review_rate, presence: true, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
+  validates :text, length: { maximum: 300 }
+
+  # アソシエーション
+  belongs_to :subscription
 end
