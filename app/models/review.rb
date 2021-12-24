6933f8502bc979f1896aef8e6cf2e482209b6801
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-24 22:52:39 +0900

    [ActiveHash]LaterCheck作成

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   # アソシエーション
   belongs_to :subscription
   has_one :action_plan, dependent: :destroy
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :later_check
 end
