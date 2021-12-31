e38e57d98acf697d9e3ecddf753826b9339a93ff
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-31 20:26:28 +0900

    [ActiveHash]Reasonの作成

---
+++ app/models/reason.rb
@@ -0,0 +1,12 @@
+class Reason < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '今の自分には活用しきれなかった' },
+    { id: 2, name: 'サービス内容に不満がある' },
+    { id: 3, name: '価格が高い' },
+    { id: 4, name: '他のサービスに乗り換える' },
+    { id: 5, name: 'あまり利用しなかった' },
+    { id: 6, name: '不要になった' }
+  ]
+  include ActiveHash::Associations
+  has_many :contract_calcels
+end
