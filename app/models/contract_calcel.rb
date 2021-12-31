6c5d68e9b35300c7cd18c20e126cacf64383a948
28niwa2108 <fle20-account@yahoo.co.jp>
2021-12-31 20:29:37 +0900

    [ContractCalcel ]アソシエーション・バリデーションの設定

--- app/models/contract_calcel.rb
+++ app/models/contract_calcel.rb
@@ -1,2 +1,18 @@
 class ContractCalcel < ApplicationRecord
+  # アソシエーション
+  belongs_to :subscription
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :reason
+
+  # バリデーション
+  with_options presence: true do
+    validates :cancel_date
+    validates :reason_id, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 1,
+      less_than_or_equal_to: 6
+    }
+  validates :cancel_comment, length: { maximum: 300 }
+  end
 end
