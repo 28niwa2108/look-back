4490e6967db166a045826cb8950079c652dc7db1
28niwa2108 <fle20-account@yahoo.co.jp>
2022-01-01 18:31:40 +0900

    ContractCalcelバリデーションの修正

--- app/models/contract_calcel.rb
+++ app/models/contract_calcel.rb
@@ -13,6 +13,6 @@ class ContractCalcel < ApplicationRecord
       greater_than_or_equal_to: 1,
       less_than_or_equal_to: 6
     }
-  validates :cancel_comment, length: { maximum: 300 }
   end
+  validates :cancel_comment, length: { maximum: 300 }
 end
