950f4b5a305bd0a3f0442940bce20aacd72f03a5
28niwa2108 <fle20-account@yahoo.co.jp>
2022-01-01 18:57:56 +0900

    ContractCancelモデル作成し直し

---
+++ spec/factories/contract_cancels.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :contract_cancel do
+    
+  end
+end
---
+++ spec/models/contract_cancel_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ContractCancel, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
