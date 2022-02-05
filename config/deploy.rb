28f1457fdc4ebc7b0d553f83464f6799ce1a5327
28niwa2108 <fle20-account@yahoo.co.jp>
2022-02-05 22:27:56 +0900

    DSLの記述修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 lock '3.16.0'
 set :application, 'look-back'
-set :https://github.com/28niwa2108/look-back.git,  'git@github.com:28niwa2108/look-back.git'
+set :repo_url,  'git@github.com:28niwa2108/look-back.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.7.4'
