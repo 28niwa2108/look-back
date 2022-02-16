16a51e19229734131247d07b867242fed8b51a26
28niwa2108 <fle20-account@yahoo.co.jp>
2022-02-16 23:21:28 +0900

    ユーザー編集機能結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -115,7 +115,7 @@ RSpec.describe 'ユーザー管理機能', type: :system do
     before do
       @user = FactoryBot.create(:user)
       @subs = FactoryBot.create(:subscription, user_id: @user.id)
-      @renewal = FactoryBot.create(:contract_renewal, subscription_id: @subs.id)
+      @renewal = FactoryBot.create(:contract_renewal, subscription_id: @subs.id, next_update_date: Date.today + 1)
     end
 
     context 'マイページに遷移できるとき' do
@@ -126,13 +126,66 @@ RSpec.describe 'ユーザー管理機能', type: :system do
         find_link('My page', href: user_path(@user)).click
         expect(current_path).to eq(user_path(@user))
         expect(page).to have_content(@subs.name)
-        expect(page).to have_content("次回更新日：#{@renewal.next_update_date}")
+        expect(page).to have_content('次回更新日：')
       end
     end
 
     context 'マイページに遷移できないとき' do
       it 'ログアウト状態では、マイページに遷移できず、ログインページに遷移する' do
         visit user_path(@user)
+        expect(current_path).to eq(new_user_session_path)
+        expect(page).to have_content('マイページログイン')
+        expect(page).to have_content('ログインもしくはアカウント登録してください。')
+      end
+    end
+  end
+
+  describe 'ユーザー編集機能' do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+
+    context 'ユーザー編集ができるとき' do
+      it '正しい情報を入力すればユーザー編集ができ、マイページに移動する' do
+        sign_in_support(@user)
+        visit edit_user_registration_path
+        expect(page).to have_content('ユーザー情報の変更')
+        expect(find('input[id="nickname"]').value).to have_content(@user.nickname)
+        expect(find('input[id="email"]').value).to have_content(@user.email)
+        fill_in 'nickname', with: ''
+        fill_in 'nickname', with: '変更ネーム'
+        fill_in 'email', with: ''
+        fill_in 'email', with: 'henkou@change'
+        fill_in 'user_current_password', with: @user.password
+        expect { find('input[type="submit"]').click }.to change { User.count }.by(0)
+        expect(current_path).to eq(user_path(@user))
+        visit edit_user_registration_path
+        expect(page).to have_content('ユーザー情報の変更')
+        expect(find('input[id="nickname"]').value).to have_content('変更ネーム')
+        expect(find('input[id="email"]').value).to have_content('henkou@change')
+      end
+    end
+
+    context 'ユーザー編集ができないとき' do
+      it '誤った情報ではユーザー編集ができず、編集ページにレンダーで戻る' do
+        sign_in_support(@user)
+        visit edit_user_registration_path
+        expect(page).to have_content('ユーザー情報の変更')
+        expect(find('input[id="nickname"]').value).to have_content(@user.nickname)
+        expect(find('input[id="email"]').value).to have_content(@user.email)
+        fill_in 'nickname', with: ''
+        fill_in 'email', with: ''
+        fill_in 'user_current_password', with: ''
+        expect { find('input[type="submit"]').click }.to change { User.count }.by(0)
+        expect(current_path).to eq(user_registration_path)
+        expect(page).to have_content('ユーザー情報の変更')
+        expect(page).to have_content('メールアドレスを入力してください')
+        expect(page).to have_content('ニックネームを入力してください')
+        expect(page).to have_content('現在のパスワードを入力してください')
+      end
+
+      it 'ログアウト状態では、ユーザー編集ページに遷移できず、ログインページに遷移する' do
+        visit edit_user_registration_path
         expect(current_path).to eq(new_user_session_path)
         expect(page).to have_content('マイページログイン')
         expect(page).to have_content('ログインもしくはアカウント登録してください。')
